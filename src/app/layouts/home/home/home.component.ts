965e4bb083183d57a9b53529e538b649e280e431
thanuthiruna <thanuthiruna@gmail.com>
2024-11-27 10:05:56 +0000

    nav bar update

--- src/app/layouts/home/home/home.component.ts
+++ src/app/layouts/home/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AdminServiceService } from '../../../services/admin-service.service';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,54 @@ import { Router } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  constructor(private router:Router){
 
-    
+  
+  brands: any[] = [];
+  modals:any[]=[];
+  cars:any[]=[];
+
+  constructor(private router:Router,
+    private adminServiceService:AdminServiceService
+
+  ){}
+
+  ngOnInit(): void {
+    this.loadBrands();
+    this.loadmodal();
+    this,this.loadcars();
   }
+
+
+  loadBrands() {
+    this.adminServiceService.getBrands().subscribe(
+      (data: any[]) => {
+        this.brands = data; 
+      },
+      (error: any) => {
+        console.error('Error fetching brands:', error);
+      }
+    );
+  };
+  loadmodal(){
+    this.adminServiceService.getModels().subscribe(
+      (data: any[]) => {
+        this.modals = data; 
+      },
+      (error: any) => {
+        console.error('Error fetching brands:', error);
+      }
+    );
+  };
+  loadcars(){
+   this.adminServiceService.getCars().subscribe(
+    (data: any[]) => {
+      this.cars = data; 
+    },
+    (error: any) => {
+      console.error('Error fetching brands:', error);
+    }
+  );
+  }
+
+
 }
